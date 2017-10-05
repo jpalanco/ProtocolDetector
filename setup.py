bd3d9295c968e9a90000006ca3562a063e81f42f
Jose Ramon Palanco <jpalanco@Joses-MacBook-Pro.local>
2017-10-05 19:45:41 +0200

    Several changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='ProtocolDetector',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='https://github.com/jpalanco/ProtocolDetector',
     license='MIT',
